1884608fbfeda34fd67edd7625c59139b315952b
Mert <114191420+lumiboi@users.noreply.github.com>
2025-08-26 23:01:15 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,11 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDrFrjWxf4pIgdLxXjoUgOgBzsvPcBnUoM",
+  apiKey: "API-KEY",
   authDomain: "pomofree-7583f.firebaseapp.com",
   projectId: "pomofree-7583f",
   storageBucket: "pomofree-7583f.firebasestorage.app",
@@ -19,6 +16,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Diğer dosyalardan erişebilmek için auth ve db'yi export ediyoruz.
+// Diğer dosyalardan erişebilmek için auth ve db'yi export etmek için.
 export const auth = getAuth(app);
 export const db = getFirestore(app);
